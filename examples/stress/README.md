5e1324ffb5cbca2982a305396b5a7695d7e783dc
Harley Swick <fc@daemonpizza.com>
2024-12-03 17:22:46 -0500

    Rename torture to stress + statement tests

---
+++ examples/stress/README.md
@@ -0,0 +1,16 @@
+# stress
+
+Set of tele modules associated with Erlang unit tests to check that compilation behavior works as expected.
+
+## Build
+
+Make sure to have a local copy of tele built.
+
+    source setup.sh
+    tele build
+
+## Test
+
+Make sure to have followed the build instructions.
+
+    tele test
---
+++ examples/stress/src/stress.erl
@@ -0,0 +1,148 @@
+-module(stress).
+
+-export([]).
+
+-ifdef(TEST).
+
+-include_lib("eunit/include/eunit.hrl").
+
+stress_basic_hello_test() ->
+    ?assertEqual(<<"Hello, World!">>, stress_basic:hello()).
+
+stress_basic_hello_atom_test() ->
+    ?assertEqual(ok, stress_basic:hello_atom()).
+
+stress_basic_hello_integer_test() ->
+    ?assertEqual(1, stress_basic:hello_integer_1()),
+    ?assertEqual(-1, stress_basic:hello_integer_2()).
+
+stress_basic_hello_list_test() ->
+    ?assertEqual([1, 2, 3, 4], stress_basic:hello_list()).
+
+stress_basic_hello_map_test() ->
+    ?assertEqual(#{<<"hello">> => <<"world">>}, stress_basic:hello_map()).
+
+stress_basic_hello_add_test() ->
+    ?assertEqual(4, stress_basic:hello_add(2, 2)).
+
+stress_basic_hello_equal_test() ->
+    ?assert(stress_basic:hello_equal()).
+
+stress_basic_hello_case_test() ->
+    ?assertEqual(one, stress_basic:hello_case(1)),
+    ?assertEqual(two, stress_basic:hello_case(2)),
+    ?assertEqual(three, stress_basic:hello_case(3)),
+    ?assertEqual(something, stress_basic:hello_case(4)),
+    ?assertEqual(something, stress_basic:hello_case(-42)).
+
+stress_basic_hello_match_sig_test() ->
+    ?assertEqual(one, stress_basic:hello_match_sig(1)),
+    ?assertEqual(two, stress_basic:hello_match_sig(2)),
+    ?assertEqual(three, stress_basic:hello_match_sig(3)),
+    ?assertEqual(something, stress_basic:hello_match_sig(4)),
+    ?assertEqual(something, stress_basic:hello_match_sig(-42)).
+
+stress_basic_hello_math_test() ->
+    ?assertEqual(0, stress_basic:hello_math(2, 2)),
+    ?assertEqual(-3, stress_basic:hello_math(3, 3)).
+
+stress_basic_hello_cons_test() ->
+    ?assertEqual([1, 2, 3], stress_basic:hello_cons(1, [2, 3])).
+
+stress_basic_hello_map_update_test() ->
+    ?assertEqual(
+        #{<<"hello">> => <<"world">>},
+        stress_basic:hello_map_update(#{}, <<"hello">>, <<"world">>)
+    ),
+
+    ?assertEqual(
+        #{<<"hello">> => <<"world">>, <<"foo">> => <<"bar">>},
+        stress_basic:hello_map_update2(#{}, <<"hello">>, <<"world">>)
+    ).
+
+stress_basic_hello_list_tuples_test() ->
+    ?assertEqual(
+        [{1, 2}, {3, 4}, {5, 6}],
+        stress_basic:hello_list_tuples()
+    ).
+
+stress_basic_hello_tuple_list_test() ->
+    ?assertEqual(
+        {[1, 2], [3, 4], [5, 6]},
+        stress_basic:hello_tuple_list()
+    ).
+
+stress_basic_hello_list_map_test() ->
+    ?assertEqual(
+        [#{<<"hello">> => <<"world">>}, #{<<"foo">> => <<"bar">>}],
+        stress_basic:hello_list_map()
+    ).
+
+stress_basic_hello_map_list_test() ->
+    ?assertEqual(
+        #{
+           <<"hello">> => [1, 2],
+           <<"world">> => [3, 4]
+        },
+        stress_basic:hello_map_list()
+    ).
+
+stress_basic_hello_call_priv_test() ->
+    ?assertEqual(42, stress_basic:hello_call_priv()).
+
+stress_basic_hello_fact_test() ->
+    ?assertEqual(1, stress_basic:hello_fact(0)),
+    ?assertEqual(2, stress_basic:hello_fact(2)).
+
+stress_basic_hello_variables_test() ->
+    ?assertEqual(9, stress_basic:hello_variables(2, 2)).
+
+stress_basic_hello_list_reverse_test() ->
+    ?assertEqual(
+      [3, 2, 1],
+      stress_basic:hello_list_reverse([1, 2, 3])
+    ).
+
+stress_basic_hello_map_match_test() ->
+    ?assertEqual(
+        <<"world">>, 
+        stress_basic:hello_map_match(#{<<"hello">> => <<"world">>})
+    ),
+    ?assertEqual(
+        #{<<"foo">> => <<"bar">>},
+        stress_basic:hello_map_match(#{<<"foo">> => <<"bar">>})
+    ).
+
+stress_basic_hello_erlang_lists_map_test() ->
+    ?assertEqual(
+        [3, 4, 5],
+        stress_basic:hello_erlang_lists_map([1, 2, 3])
+    ).
+
+stress_basic_hello_fun_val_test() ->
+    F = fun () -> 42 end,
+    ?assertEqual(42, stress_basic:hello_fun_val(F)).
+
+stress_basic_hello_mod_fun_val_test() ->
+    ?assertEqual(42, stress_basic:hello_mod_fun_val(stress_basic, hello_call_me)).
+
+stress_basic_try_catch_test() ->
+    ?assertEqual(42, stress_basic:hello_try_catch()).
+
+stress_statements_add2_test() ->
+    ?assertEqual(6, stress_statements:add2_example()).
+
+stress_statements_point_test() ->
+    P = stress_statements:point_new(0, 0),
+    P2 = stress_statements:point_down(P),
+    P3 = stress_statements:point_up(P2),
+    P4 = stress_statements:point_left(P3),
+    P5 = stress_statements:point_right(P4),
+
+    ?assertEqual({point, 0, 0}, P5).
+
+stress_statements_point_default_test() ->
+    P = stress_statements:point_default(),
+    ?assertEqual({point, 0, 0}, P).
+
+-endif.
